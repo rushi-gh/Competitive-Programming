14a11c987c868e13cd0816ac99d02e1cd1e11d4f
Rushikesh Naikwadi <rushee.mailme@gmail.com>
2024-06-19 21:38:15 +0530

    Power of 2 and 4

--- Bit_Manipulation.cpp
+++ Bit_Manipulation.cpp
@@ -37,6 +37,20 @@ int ClearBitsInRange(int n, int l, int r) {
     return n;
 }
 
+string IsPowerOf2 (int n) {
+    return (n > 0 and (n & (n - 1)) == 0 ? "Yes" : "No");
+}
+
+string IsPowerOf4(int n) {
+    return ((n > 0 and (n & (n - 1)) == 0 and n % 3 == 1) ? "Yes" : "No");
+
+    // int mask = 0;
+    // for (int j = 0; j < 32; j += 2) {
+    //     mask |= (1LL << j);
+    // }
+    // return IsPowerOf2(n & mask);
+}
+
 int main() {
     // cout << EvenOrOdd(1) << ' ' << EvenOrOdd(2) << endl;
 
@@ -45,6 +59,9 @@ int main() {
     // cout << ClearIthBit(6, 0) << ' ' << ClearIthBit(6, 1) << endl;
     // cout << ClearLastIBits(15, 0) << ' ' << ClearLastIBits(15, 1) << ' ' << ClearLastIBits(15, 2) << endl;
     // cout << ClearBitsInRange(15, 1, 2) << endl;
+
+    // cout << IsPowerOf2(0) << ' ' << IsPowerOf2(1) << ' ' << IsPowerOf2(2) << ' ' << IsPowerOf2(3) << endl;
+    // cout << IsPowerOf4(0) << ' ' << IsPowerOf4(1) << ' ' << IsPowerOf4(2) << ' ' << IsPowerOf4(4) << endl;
 
     return 0;
 }
