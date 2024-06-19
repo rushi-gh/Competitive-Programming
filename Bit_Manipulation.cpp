90a0124c95d1b1fd79124af010d68e7605ee33a2
Rushikesh Naikwadi <rushee.mailme@gmail.com>
2024-06-19 20:50:27 +0530

    Check Even or Odd using Bitwise AND

--- Bit_Manipulation.cpp
+++ Bit_Manipulation.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string EvenOrOdd (int n) {
+    return (n & 1) ? "Odd" : "Even";
+}
+
 int main() {
-    int n;
-    cin >> n;
-    cout << n << endl;   
+    // cout << EvenOrOdd(1) << ' ' << EvenOrOdd(2) << endl;
+    return 0;
 }
